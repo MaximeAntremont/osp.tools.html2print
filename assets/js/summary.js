beeb0e3e4bbcbfd5bfea7343acb311ce582f42aa
MaximeAntremont <maxime.antremont@gmail.com>
2016-05-01 12:34:48 +0200

    Ajout de la pagination

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -12,10 +12,35 @@ var summary_selector = "#summary",
     list_children_element = "li",
 
     $summary = $(summary_selector),
-    $sl = $summary.append($(wrap(list_element))).addClass(list_element_class),
+    $sl = $(wrap(list_element)).addClass(list_element_class),
     $parties = $("body h1");
 
-$parties.each(function(){
+    $summary.append($sl);
+
+console.log($parties);
+
+$parties.each(function(index){
     var self = this;
-    $sl.append($(wrap(list_children_element, self.textContent)));
+    var $self = $(self);
+
+    var interval = setInterval(
+        function(){
+            var fragment = $self.attr("data-css-regions-fragment-source");
+
+            if(typeof(fragment) != 'undefined'){
+                var $current_list_element = $(wrap(list_children_element, self.textContent)),
+                    $page_indicator = $(wrap("span")),
+                    parent_page = $("[data-css-regions-fragment-of='"+fragment+"'"),
+                    page_number = parent_page.parents("div.paper[id^='page']").attr("id").match(/\d{1,5}/g)[0];
+                $page_indicator.addClass("summary_element");
+                $page_indicator.text(page_number);
+
+                $current_list_element.append($page_indicator);
+
+                $sl.append($current_list_element);
+
+                window.clearInterval(interval);
+            }
+        }
+    ,1000);
 });
